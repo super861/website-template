9ae1eccc958b003fb0218ce1e1771409e24d06b9
Koen <koenr14@live.nl>
2018-05-16 16:55:36 +0200

    footer header main update

--- src/js/scenes/components/Footer.js
+++ src/js/scenes/components/Footer.js
@@ -1,9 +1,12 @@
+// src/js/scenes/components/Footer.js
+
 import React from 'react';
 
 const Footer = () => (
   <footer className="footer">
     <div className="container">
       <span className="text-muted">&#169; Copyright information and whatnot!</span>
+      <a className="text-muted float-right" href="https://github.com/super861/website-template">Github</a>
     </div>
   </footer>
 );
--- src/js/scenes/components/Header.js
+++ src/js/scenes/components/Header.js
@@ -1,3 +1,5 @@
+// src/js/scenes/components/Header.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
