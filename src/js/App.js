1e054d747fe53364c91f295e86485c7c1ae98d12
Koen <koenr14@live.nl>
2018-05-17 09:59:35 +0200

    basic setup

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,16 @@
 // src/js/App.js
 
 import React, { Component } from 'react';
-import Header from './scenes/components/Header.js';
-import Footer from './scenes/components/Footer.js';
+import Header from './scenes/Header.js';
+import Footer from './scenes/Footer.js';
+import Main from './scenes/Main.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Main />
         <Footer />
       </div>
     );
